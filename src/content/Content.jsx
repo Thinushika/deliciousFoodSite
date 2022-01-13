557dab02f73cdca1ea9d20a4cc88f71cc356bca7
Thinushi <thinupavani@gmail.com>
2022-01-13 23:53:23 +0530

    update

--- src/content/Content.jsx
+++ src/content/Content.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import "./Content.css";
 
 function Content() {
   return (
     <>
       <div className="bg">
-        <img src="./f1.png" alt="food" className="backgroundImage" />
+        <div className="text">
+        <h1 className="mainText">BEST FOOD FOR </h1>
+        <h1 className="mainText">YOUR TASTE </h1>
+        <p className="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorum unde voluptatum at atque, deserunt perspiciatis minima</p>
+        </div>
+        <div className="bgImage">
+        <img src="./f2.png" alt="food" className="backgroundImage" />
+        </div>
       </div>
     </>
   );
