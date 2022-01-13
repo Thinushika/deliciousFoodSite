ca0af8a20be6faa734416f9932619d49b730a98c
Thinushi <thinupavani@gmail.com>
2022-01-14 00:17:21 +0530

    update

--- src/content/Content.jsx
+++ src/content/Content.jsx
@@ -9,6 +9,10 @@ function Content() {
         <h1 className="mainText">BEST FOOD FOR </h1>
         <h1 className="mainText">YOUR TASTE </h1>
         <p className="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorum unde voluptatum at atque, deserunt perspiciatis minima</p>
+        <div className="btnDiv">
+        <button className="startedBtn">GET STARTED</button>
+        <button className="startedBtn">GET STARTED</button>
+        </div>
         </div>
         <div className="bgImage">
         <img src="./f2.png" alt="food" className="backgroundImage" />
