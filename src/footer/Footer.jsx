cb855f3612e6d670f97dd1db63673ac499551805
Thinushi <thinupavani@gmail.com>
2022-01-14 22:04:58 +0530

    update

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import "./Footer.css";
+import { FaFacebookSquare, FaLinkedin, FaInstagramSquare, FaTwitterSquare} from "react-icons/fa";
+
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="footerContainer">
+            <div className="leftC">
+            <img src="../logo.png" className="logoside" alt=" Delicious" />
+            </div>
+            <div className="centerC">
+                <div className="detailL">
+                    <p>Location</p>
+                    <p>Menu</p>
+                    <p>About</p>
+                </div>
+                <div className="detailR">
+                    <p>Contact</p>
+                    <p>+94 000 000 00</p>
+                    <p>info@delicious.com</p>
+                </div>
+            </div>
+            <div className="rightC">
+                <FaFacebookSquare />
+                <FaInstagramSquare />
+                <FaLinkedin />
+                <FaTwitterSquare />
+            </div>
         </div>
     )
 }
--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Form.css";
 
 const Form = () => {
-    return (
-        <div>
-            Form
+  return (
+    <div className="formContainer">
+      <img src="./formbackground.jpg" alt="background" className="formImg" />
+      <div className="ttl">
+        <h1 className="formTitle">JOIN US AND GET</h1>
+        <h1 className="formTitle">DISCOUNT UP TO 50%</h1>
+        <div className="inputField">
+          <input
+            type="text"
+            placeholder="Your email here"
+            className="inpFeild"
+          />
         </div>
-    )
-}
+        <div className="formLink">
+          <a href="#" className="linkS">I'm new member</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Form
+export default Form;
